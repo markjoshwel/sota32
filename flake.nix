de6179c235492a0120e5254ca5bc21025e72131f
Mark Joshwel <mark@joshwel.co>
2026-01-23 15:05:19 +0800

    ci: add nix flake for restepper

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "flake for running the sota staircase ReStepper";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs; {
+        devShells.default = mkShellNoCC {
+          buildInputs = [
+            git
+            git-lfs
+            git-filter-repo
+            (python313.withPackages (python-pkgs: [
+              python-pkgs.tqdm
+            ]))
+          ];
+        };
+      }
+    );
+}
